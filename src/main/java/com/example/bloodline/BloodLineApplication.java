28bcb5178dfa521c8c9e47269df7a77018082fec
박예진 <qkrdpwls2002>
2025-04-14 20:05:38 +0900

    🎉 Begin : 프로젝트 생성

---
+++ src/main/java/com/example/bloodline/BloodLineApplication.java
@@ -0,0 +1,13 @@
+package com.example.bloodline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodLineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodLineApplication.class, args);
+	}
+
+}
